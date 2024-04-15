79325a25c44813a2c42d629ce2e8652ec4d36eba
CeciliaB22 <ceciliabalaban11@gmail.com>
2024-04-15 11:12:57 +0200

    exercise three

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-let squaresContainer = document.querySelector(".squaresContainer")
+let squaresContainer = document.querySelector(".squaresContainer");
+//let squareHTML = "<div class ='redSquare'></div>";
 function change() {
     // add red squares to the HTML when the button is clicked
-    squaresContainer.innerHTML
+    //squaresContainer.innerHTML += squareHTML; // solucion 1
+    squaresContainer.innerHTML += "<div class ='redSquare'></div>"; // solucio 2
 }
